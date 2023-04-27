407079e7b9450a6d462b2d78c80101c515ac281e
Ma6upa <vaud55@gmail.com>
2023-04-27 14:47:22 +0400

    add data to root component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    Работает
+    <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+    <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
   </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 2,
+    }
+  }
+}
 </script>
 
 <style>
