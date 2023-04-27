c3da7d9499daa95d7078e5ef4631d55bf3794193
Ma6upa <vaud55@gmail.com>
2023-04-27 15:35:53 +0400

    add writing posts methods

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input 
+        v-bind:value="title"
+        @input="title = $event.target.value" 
+        class="input" 
+        type="text" 
+        placeholder="Название"
+      >
+      <input 
+        v-bind:value="body" 
+        @input="body = $event.target.value" 
+        class="input" 
+        type="text" 
+        placeholder="Описание"
+      >
+      <button 
+        class="btn" 
+        @click="createPost"
+      >
+        Создать
+      </button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название: </strong> {{ post.title }} </div>
       <div><strong>Описание: </strong> {{ post.body }} </div>
@@ -12,14 +35,23 @@ export default {
   data() {
     return {
       posts: [
-        {id: 1, title: 'JavaScript', body: 'JS - язык программирования'},
-        {id: 2, title: 'Чай', body: 'Чай - единственный допустимый напиток'},
-        {id: 3, title: 'УдГУ', body: 'УдГУ - вроде как вуз'},
-      ]
+        { id: 1, title: 'JavaScript', body: 'JS - язык программирования' },
+        { id: 2, title: 'Чай', body: 'Чай - единственный допустимый напиток' },
+        { id: 3, title: 'УдГУ', body: 'УдГУ - вроде как вуз' },
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
-    
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost)
+    },
   }
 }
 </script>
@@ -31,9 +63,34 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
 .post {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 10px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
 }
 </style>
