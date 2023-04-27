9ee14d580e1ead556568926ed667041164f42a63
Ma6upa <vaud55@gmail.com>
2023-04-27 14:49:47 +0400

    add event handlers changing data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div>
+      <button v-on:click="addLike">Лайк</button>
+      <button  v-on:click="addDislike">Дизлайк</button>
+    </div>
     <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
     <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
   </div>
@@ -10,7 +14,15 @@ export default {
   data() {
     return {
       likes: 0,
-      dislikes: 2,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes +=1
+    },
+    addDislike() {
+      this.dislikes +=1
     }
   }
 }
