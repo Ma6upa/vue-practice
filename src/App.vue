612897b9941901a55855806f83b2ad889a61c07d
Ma6upa <vaud55@gmail.com>
2023-04-28 14:42:50 +0400

    add basic components decomposition

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
+@@ -1,98 +0,0 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input 
-        v-bind:value="title"
-        @input="title = $event.target.value" 
-        class="input" 
-        type="text" 
-        placeholder="Название"
-      >
-      <input 
-        v-bind:value="body" 
-        @input="body = $event.target.value" 
-        class="input" 
-        type="text" 
-        placeholder="Описание"
-      >
-      <button 
-        class="btn" 
-        @click="createPost"
-      >
-        Создать
-      </button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название: </strong> {{ post.title }} </div>
-      <div><strong>Описание: </strong> {{ post.body }} </div>
-    </div>
+    <PostForm @create="createPost" />
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
 export default {
+  components: {
+    PostList,
+    PostForm,
+  },
   data() {
     return {
       posts: [
@@ -44,15 +26,8 @@ export default {
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body
-      }
-      this.posts.push(newPost)
-      this.title =''
-      this.body = ''
+    createPost(post) {
+      this.posts.push(post);
     },
   }
 }
@@ -67,32 +42,5 @@ export default {
 
 .app {
   padding: 20px
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 10px;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-
-.btn {
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
