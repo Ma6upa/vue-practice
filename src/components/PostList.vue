120beb8cb5101d8a003b0f142db46e4a270d5f94
Ma6upa <vaud55@gmail.com>
2023-05-02 21:17:29 -0700

    add simple animation on post actions using TransitionGroup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <PostItem 
-      v-for="post in posts" 
-      :post="post" 
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <TransitionGroup name="post-list">
+      <PostItem 
+        v-for="post in posts" 
+        :post="post" 
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </TransitionGroup>
   </div>
   <h2 v-else>
     Списпок постов пуст
@@ -14,6 +16,7 @@
 </template>
 
 <script>
+import { TransitionGroup } from 'vue';
 import PostItem from './PostItem.vue';
 
 export default {
@@ -23,8 +26,18 @@ export default {
       required: true
     }
   },
-  components: { PostItem }
+  components: { PostItem, TransitionGroup }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+</style>
