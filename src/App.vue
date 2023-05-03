9ce2cf0badb86fa020d99b8a4e3bf4f5bada624d
Ma6upa <vaud55@gmail.com>
2023-05-03 17:54:57 +0400

    add basic routing with vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,13 @@
-@@ -1,98 +0,0 @@
 <template>
   <div class="app">
-    <h1>Страница с постами</h1>
-    <MyInput 
-      v-model="searchQuery"
-      placeholder="Поиск....."
-    />
-    <div class="app__btns">
-      <MyButton @click="showDialog">
-        Создать пост
-      </MyButton>
-      <MySelect v-model="selectedSort" :options="sortOptions"/>
-    </div>
-    <MyDialog v-model:show="dialogVisible">
-      <PostForm @create="createPost" />
-    </MyDialog>
-    <PostList :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostsLoading" />
-    <h2 v-else>Идет загрузка...</h2>
-    <div ref="observer" class="observer"></div>
+    <RouterView>
+      
+    </RouterView>
   </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm.vue"
-import PostList from "@/components/PostList.vue"
-import MyDialog from "./components/UI/MyDialog.vue";
-import MyButton from "./components/UI/MyButton.vue";
-import axios from "axios";
-import MySelect from "./components/UI/MySelect.vue";
-import MyInput from "./components/UI/MyInput.vue";
 
-export default {
-  components: {
-    PostList,
-    PostForm,
-    MyDialog,
-    MyButton,
-    MySelect,
-    MyInput
-  },
-  data() {
-    return {
-      posts: [],
-      dialogVisible: false,
-      isPostsLoading: false,
-      selectedSort: '',
-      searchQuery: '',
-      page: 1,
-      limit: 10,
-      totalPages: 0,
-      sortOptions: [
-        {value: 'title', name: 'По названию'},
-        {value: 'body', name: 'По описанию'}
-      ]
-    }
-  },
-  methods: {
-    createPost(post) {
-      this.posts.push(post);
-      this.dialogVisible = false;
-    },
-    removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
-    },
-    showDialog() {
-      this.dialogVisible = true
-    },
-    async fetchPosts() {
-      try {
-        this.isPostsLoading = true
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        })
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = response.data
-      } catch (e) {
-        alert('Ошибка')
-      } finally {
-        this.isPostsLoading = false
-      }
-    },
-    async loadMorePosts() {
-      try {
-        this.page += 1
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        })
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = [...this.posts, ...response.data]
-      } catch (e) {
-        alert('Ошибка')
-      }
-    },
-  },
-  mounted() {
-    this.fetchPosts()
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    }
-    const observer = new IntersectionObserver(callback, options)
-    observer.observe(this.$refs.observer)
-  },
-  computed: {
-    sortedPosts() {
-      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-      )
-    },
-    sortedAndSearchedPosts() {
-      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    },
-  },
-  watch: {}
-}
 </script>
 
 <style>
@@ -134,26 +18,5 @@ export default {
 }
 .app {
   padding: 20px
-}
-.app__btns {
-  display: flex;
-  justify-content: space-between;
-  margin:15px 0;
-}
-.page__wrapper {
-  display: flex;
-  flex-direction: row;
-  margin-top: 15px;
-}
-.page {
-  border: 1px solid black;
-  padding: 10px;
-  cursor: pointer;
-}
-.current-page {
-  border: 2px solid teal;
-}
-.observer {
-
 }
 </style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Главная страница</h1>
+    <RouterLink to="/posts">Посты</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
