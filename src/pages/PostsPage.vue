17833292c2b069f8cc14a018b3b2eecd27af753b
Ma6upa <vaud55@gmail.com>
2023-05-04 16:13:55 +0400

    implement store into the PostsPage (1/2)

--- src/pages/PostsPage.vue
+++ src/pages/PostsPage.vue
@@ -2,15 +2,15 @@
 <template>
   <div>
     <h1>Страница с постами</h1>
-    <MyInput 
+    <!-- <MyInput 
       v-model="searchQuery"
       placeholder="Поиск....."
-    />
+    /> -->
     <div class="app__btns">
       <MyButton @click="showDialog">
         Создать пост
       </MyButton>
-      <MySelect v-model="selectedSort" :options="sortOptions"/>
+      <!-- <MySelect v-model="selectedSort" :options="sortOptions"/> -->
     </div>
     <MyDialog v-model:show="dialogVisible">
       <PostForm @create="createPost" />
@@ -22,9 +22,9 @@
 </template>
 
 <script>
-import axios from "axios";
 import PostForm from "@/components/PostForm.vue"
 import PostList from "@/components/PostList.vue"
+import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
 
 export default {
   components: {
@@ -33,21 +33,17 @@ export default {
   },
   data() {
     return {
-      posts: [],
       dialogVisible: false,
-      isPostsLoading: false,
-      selectedSort: '',
-      searchQuery: '',
-      page: 1,
-      limit: 10,
-      totalPages: 0,
-      sortOptions: [
-        {value: 'title', name: 'По названию'},
-        {value: 'body', name: 'По описанию'}
-      ]
     }
   },
   methods: {
+    ...mapMutations({
+      setPage: 'post/setPage'
+    }),
+    ...mapActions({
+      loadMorePosts: 'post/loadMorePosts',
+      fetchPosts: 'post/fetchPosts'
+    }),
     createPost(post) {
       this.posts.push(post);
       this.dialogVisible = false;
@@ -58,52 +54,26 @@ export default {
     showDialog() {
       this.dialogVisible = true
     },
-    async fetchPosts() {
-      try {
-        this.isPostsLoading = true
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        })
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = response.data
-      } catch (e) {
-        alert('Ошибка')
-      } finally {
-        this.isPostsLoading = false
-      }
-    },
-    async loadMorePosts() {
-      try {
-        this.page += 1
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        })
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = [...this.posts, ...response.data]
-      } catch (e) {
-        alert('Ошибка')
-      }
-    },
+  },
+  computed: {
+    ...mapState({
+      posts: state => state.post.posts,
+      isPostsLoading: state => state.post.isPostsLoading,
+      selectedSort: state => state.post.selectedSort,
+      searchQuery: state => state.post.searchQuery,
+      page: state => state.post.page,
+      limit: state => state.post.limit,
+      totalPages: state => state.post.totalPages,
+      sortOptions: state => state.post.sortOptions,
+    }),
+    ...mapGetters({
+      sortedPosts: 'post/sortedPosts',
+      sortedAndSearchedPosts: 'post/sortedAndSearchedPosts'
+    }),
   },
   mounted() {
     this.fetchPosts()
   },
-  computed: {
-    sortedPosts() {
-      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-      )
-    },
-    sortedAndSearchedPosts() {
-      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    },
-  },
-  watch: {}
 }
 </script>
 
@@ -111,20 +81,24 @@ export default {
 .app__btns {
   display: flex;
   justify-content: space-between;
-  margin:15px 0;
+  margin: 15px 0;
 }
+
 .page__wrapper {
   display: flex;
   flex-direction: row;
   margin-top: 15px;
 }
+
 .page {
   border: 1px solid black;
   padding: 10px;
   cursor: pointer;
 }
+
 .current-page {
   border: 2px solid teal;
 }
+
 .observer {}
 </style>
